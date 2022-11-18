d4a88277e1eeeb4049eb0ccc31f8a3fe79548115
daaku <tonycadahia@gmail.com>
2022-11-18 13:42:58 -0500

    add_render

--- UI/_render.py
+++ UI/_render.py
@@ -1,14 +1,36 @@
 import sys
 import pygame
+from _grid import get_grid
 
-def start(condition) -> None:
-    """
-    Este método se encarga de empezar la simulación despues de que pygame haya iniciado
+class Render:
+    def __init__(self, height: int=600, width: int=800) -> None:
+        """
+        inicializador de clase, crea un screen de dimensiones (width, height)
 
-    condition -> condición de parada de la simulación
-    """
-    while condition:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        # more code
+        height -> alto del screen, width -> ancho del screen
+        """
+        self.__screen = pygame.display.set_mode(size=(width, height))
+        self.background = get_grid(height, width)
+
+    def clean(self):
+        """
+        clean limpia toda la imagen que se muestra en el screen de pygame
+        """
+        self.__screen.fill(self.background)
+
+    def start(self, condition) -> None:
+        """
+        start se encarga de empezar la simulación despues de que pygame haya iniciado
+
+        condition -> condición de parada de la simulación
+        """
+        while condition:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            #################################
+            #      GET STATE CODE HERE      #
+            #################################
+            self.clean()
+            self.__screen.blit(ball, ballrect)
+            pygame.display.flip()
