e26a82834b7fa7d3b9502ec08be5f77bbf444817
daaku <tonycadahia@gmail.com>
2022-11-19 16:59:28 -0500

    fix_render_iter

--- UI/_grid.py
+++ UI/_grid.py
@@ -3,6 +3,18 @@ from enum import Enum
 
 celd_type = Enum('celd_type', 'unit empty')
 
+def get_color(celd: celd_type) -> tuple[int, int, int]:
+    """
+    Dado un tipo de celda retorna su representante en color
+
+    return -> tuple[int, int, int]
+    """
+    if celd is celd_type.empty:
+        return (0, 0, 0)
+    if celd is celd_type.unit:
+        return (0, 0, 0)
+    raise NotImplementedError("celd type indefinido")
+
 def get_grid(height: int, width: int) -> np.matrix:
     """
     Crea una grilla a partir de las dimensiones que se pasan en el método
--- UI/_render.py
+++ UI/_render.py
@@ -1,18 +1,20 @@
 import sys
 import pygame
-from typing import Iterable
 from _grid import get_grid
+import numpy as np
+from typing import Iterable
 
 
 class Render:
-    def __init__(self, height: int = 600, width: int = 800) -> None:
+    def __init__(self, condition, last_state: np.matrix = None, height: int = 600, width: int = 800) -> None:
         """
         inicializador de clase, crea un screen de dimensiones (width, height)
 
         height -> alto del screen, width -> ancho del screen
         """
+        self.condition = condition
         self.__screen = pygame.display.set_mode(size=(width, height))
-        self.background = get_grid(height, width)
+        self.last_state = get_grid(height, width) if last_state is None else last_state
 
     def __iter__(self) -> Iterable:
         """
@@ -20,29 +22,34 @@ class Render:
 
         return -> Iterable(States)
         """
-        raise StopIteration("not implemented")
-
-    def start(self, condition) -> None:
-        """
-        start se encarga de empezar la simulación después de que pygame haya iniciado
-
-        condition -> condición de parada de la simulación
-        """
-        while condition:
+        while self.condition:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            self.update()
+            #################################
+            #      GET STATE CODE HERE      #
+            #################################
+            yield self.last_state # new state
 
-    def update(self) -> bool:
+    def start(self) -> None:
+        """
+        start se encarga de recorrer la simulación hasta el final después de que pygame haya iniciado
+
+        condition -> condición de parada de la simulación
+        """
+        for i in self:
+            self.update(i)
+
+    def update(self, state: np.matrix) -> bool:
         """
         update se encarga de actualizar la imagen que se muestra en el screen de pygame
 
         return -> bool
         """
-        #################################
-        #      GET STATE CODE HERE      #
-        #################################
+        # for item1, item2 in zip(self.last_state, state):
+        #     for value1, value2 in zip(item1, item2):
+        #         if(value1 is not value2):
+        #             #change frame
         return False
 
     def clean(self) -> None:
