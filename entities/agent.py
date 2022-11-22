207f296b242bbbbd7c7bd3aea98b4d34cea47509
Alejandra <amonzon1113@gmail,com>
2022-11-22 04:00:52 -0500

    RandomBehavior impemented

--- entities/agent.py
+++ entities/agent.py
@@ -1,24 +1,49 @@
 from .connector import StateMannager as S
+from .connector import DIRECTIONS, STATES
+from random import randint
 
+def int_to_direction(direction):
+    if direction == 0:
+        return DIRECTIONS.N
+    elif direction == 1:
+        return DIRECTIONS.NE
+    elif direction == 2:
+        return DIRECTIONS.E
+    elif direction == 3:
+        return DIRECTIONS.SE
+    elif direction == 4:
+        return DIRECTIONS.S
+    elif direction == 5:
+        return DIRECTIONS.SW
+    elif direction == 6:
+        return DIRECTIONS.W
+    elif direction == 7:
+        return DIRECTIONS.NW
+    
 class Agent:   
     __id = 0
     def __init__(self, ia) -> None:
         self.connectors = {}
-        __id +=1
-        self.__id =__id 
+        Agent.__id +=1
+        self.__id =Agent.__id 
         self.ia = ia
     
     def connect(self, connector:S.Connector):
-        self.connectors[connector.__id]= connector
+        self.connectors[connector._Connector__id]= connector
     
     def move_connectors(self,map_copy):
-        for id,c in self.connectors:
+        for id,c in self.connectors.items():
             if not c.is_connected(): 
                 self.connectors.__delitem__(id)
             else:
                 self.ia.get_move_for(c,map_copy)
         
-    
+
+class RandomMove_IA:
+    def get_move_for(self,connector:S.Connector, map):
+        if(connector.state == STATES.Stand):
+            dir = randint(0,7)
+            connector.move_notifier(connector,int_to_direction(dir))
 
                 
             
--- entities/connector.py
+++ entities/connector.py
@@ -32,7 +32,7 @@ def discrete_distance(x_0,y_0,x_1,y_1):
 class StateMannager:
     class Connector:
         __id = 0
-        def __init__(self,agent, unit:Unit, possition, functions:list[function]) -> None:
+        def __init__(self,agent, unit:Unit, possition, functions) -> None:
             self.agent = agent
             self.unit = unit 
             self.__current_hp = unit.GetHealthPoints
@@ -61,7 +61,7 @@ class StateMannager:
             return self.__current_hp
 
         def is_connected(self):
-            return self.__current_hp <= 0 
+            return self.__current_hp > 0 
     
     def __init__(self, map, agents_units_possitions):
         self.map = map
@@ -73,9 +73,8 @@ class StateMannager:
         for ag, units in agents_units_possitions:
             self.agents.append(ag)
             for unit,possition in units:
-                connector = self.create_connector(unit,possition)
+                connector = self.create_connector(ag,unit,possition)
                 ag.connect(connector)
-                self.connectors.append(ag,connector)
         
         
                 
