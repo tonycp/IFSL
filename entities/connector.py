7ef86317e7e0c73cbb086f2840564313f409271b
leo00010011 <ulloaferrerleonardo@gmail.com>
2022-12-23 23:10:03 -0500

    Fixed collision of movements

--- entities/connector.py
+++ entities/connector.py
@@ -74,8 +74,18 @@ class StateMannager:
         return log
 
     def __exec_actions(self):
-        #ejecutando movimientos
         log = []
+        self.exec_move(log)
+        self.exec_swap(log)
+        self.exec_attack(log)
+        self.move_notifications = []
+        self.attack_notifications = []
+        self.swap_notifications = []
+        return log
+
+    def exec_move(self,log):
+        collisions = dict()
+        end_movements = []
         for connector, num_dir in self.move_notifications:
             if(not connector.prev_dir or direction_to_int(connector.prev_dir) != num_dir or connector.state != STATES.Moving):
                 connector.timer = connector.unit.get_move_cost
@@ -86,15 +96,46 @@ class StateMannager:
                 i, j = connector.get_position()
                 new_i = i + I_DIR[num_dir]
                 new_j = j + J_DIR[num_dir]
+                end_movements.append(connector)
                 if self.map[new_i,new_j].is_empty:
                     self.map[i,j].set_unit()
                     self.map[new_i,new_j].set_unit(connector)
                     log.append(self.map[i,j])
                     log.append(self.map[new_i,new_j])
                     connector.update_position((new_i,new_j))
-                connector.state = STATES.Stand
-                connector.prev_dir = None
-        #ejecutando swap
+                else:
+                    arr = collisions.get((new_i,new_j))
+                    if arr:
+                        arr.append(connector)
+                    else:
+                        collisions[(new_i,new_j)] = [self.map[new_i,new_j].get_unit,connector]
+
+        for pos, connector_list in collisions.items:
+            ran = randint(1,sum([con.unit.get_move_cost for con in connector_list])) 
+            for con in connector_list:
+                ran -= con.unit.get_move_cost
+                if ran <= 0:
+                    if(con.get_position() != pos):
+                        #poner al que se había usado para marcar a su posicion original
+                        old_con = self.map[pos].get_unit
+                        self.map[pos].set_unit()
+                        old_i = pos[0] + I_DIR[(direction_to_int(old_con.prev_dir) + 4)%4]
+                        old_j = pos[1] + J_DIR[(direction_to_int(old_con.prev_dir) + 4)%4]
+                        self.map[(old_i,old_j)].set_unit(old_con)
+                        #poner al que es en su posicion correcta
+                        self.map[con.get_position()].set_unit()
+                        self.map[pos].set_unit(connector)
+                        log.append(self.map[i,j])
+                        log.append(self.map[new_i,new_j])
+                        connector.update_position((new_i,new_j))
+                    break
+
+
+        for end_mov_connector in end_movements:
+            end_mov_connector.state = STATES.Stand
+            connector.prev_dir = None
+    
+    def exec_swap(self, log):
         for connector,other_connector, num_dir in self.swap_notifications:
             i, j = connector.get_position()
             new_i = i + I_DIR[num_dir]
@@ -121,7 +162,8 @@ class StateMannager:
                 other_connector.update_position(connector)
                 log.append(connector.get_position())
                 log.append(other_connector.get_position())
-        # ejecutando ataques
+    
+    def exec_attack(self,log):
         for connector, pos_i, pos_j in self.attack_notifications:
             other_connector = self.map[pos_i,pos_j]
             was_alive = other_connector.is_connected()
@@ -134,11 +176,8 @@ class StateMannager:
 
             if(was_alive and not other_connector.is_connected or not was_alive and other_connector.is_connected):
                 log.append(other_connector.get_position())
-        self.move_notifications = []
-        self.attack_notifications = []
-        self.swap_notifications = []
-        return log
-    
+
+
     def is_valid_move(self,connector: Connector,num_dir : int):
         i, j = connector.get_position()
         new_i = i + I_DIR[num_dir]
@@ -146,7 +185,7 @@ class StateMannager:
         height, width = self.map.shape
         if not validMove(new_i, new_j, height, width): return False
         cell_to_move = self.map[new_i,new_j]
-        return cell_to_move.crossable(connector.unit)
+        return cell_to_move.crossable(connector.unit) and not cell_to_move.get_unit
         
     
     def notify_move(self, connector : Connector, direction):
