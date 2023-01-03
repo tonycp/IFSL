a8a506e9097bc4e4ad5d5a63c5b50f29812940d5
leo00010011 <ulloaferrerleonardo@gmail.com>
2023-01-02 21:06:41 -0500

    finished_GA

--- Tester/test.py
+++ Tester/test.py
@@ -4,6 +4,7 @@ import numpy as np
 from entities._units import Fighter
 import entities.agent  as A
 from entities.connector import StateMannager as SM
+from IA.basicAlgorithms import *
 
 
 # item = Fighter()
@@ -12,6 +13,13 @@ from entities.connector import StateMannager as SM
 # agent.move_connectors(None)
 # print(item.get_attack_range)
 
-state = np.ndarray(shape= 2, dtype=tuple)
-state[0] = (1,2)
-print(state)
+cell_info = []
+distance = create_min_distance((0,0))
+GA(pob_generator= create_knuth_shuffle_pob_generator(len(cell_info),20),
+   parents_selector= RouletteWheel_ParentSelector,
+   crossover_operator= create_crossover_operator(create_crossover_cost(distance)),
+   mutation_operator= mutation_sequence_creator([swap_mutator,create_two_opt_mutator(distance)]),
+   fitness_func= create_fitness_func(distance),
+   cell_info = cell_info,
+   top_generation= 20
+    )
